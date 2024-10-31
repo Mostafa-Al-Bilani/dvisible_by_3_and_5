8539e39358a3bd5dcd16958b0cbfa05dd3a5d53a
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-10-31 12:12:21 +0200

    update

--- divisible_by_3and_5.py
+++ divisible_by_3and_5.py
@@ -1,5 +1,8 @@
 number = int(input("choose a number:"))
+numbers_divisble = []
 
-for i in range (number + 1): #We use number + 1 in the range to include the number chosen by the user
+for i in range (1, number + 1): #We use number + 1 in the range to include the number chosen by the user
     if i % 3 == 0 or i % 5 == 0: #we check if the number is divisble by 3 and 5 using the % remainder operator
-        print(i)
+        numbers_divisble.append(i)
+
+print("The numbers divisble by 3 and 5 are:", numbers_divisble)
